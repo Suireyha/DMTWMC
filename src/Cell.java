270db701a522389a619baf34004c5ad31069cfb4
Suireyha <Marvink2296@gmail.com>
2025-08-31 11:50:01 +1000

    Changed the colour of the grid with new variable gridColor and added stroke to cell borders to make the grid lines thicker

--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,13 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Graphics2D; //Imported by Marvin to edit stroke value of cell borders
 
 public class Cell extends Rectangle {
+  Color gridColor = new Color(57, 58, 59);
+  Color highlighted = new Color(127, 128, 129);
   static int size = 35;
 
   public Cell(int x, int y) {
@@ -12,13 +16,18 @@ public class Cell extends Rectangle {
 
   public void paint(Graphics g, Point mousePos) {
     if(contains(mousePos)) {
-      g.setColor(Color.GRAY);
+      g.setColor(highlighted);
     } else {
       g.setColor(Color.WHITE);
     }
+
     g.fillRect(x, y, size, size);
-    g.setColor(Color.BLACK);
-    g.drawRect(x, y, size, size);
+
+    Graphics2D g2d = (Graphics2D) g; //Casting our graphics object to a g2d so we can add stroke below
+
+    g2d.setColor(gridColor); //Colour of the grid borders
+    g2d.setStroke(new BasicStroke(1.5f));
+    g2d.drawRect(x, y, size, size);
   }
 
   public boolean contains(Point p) {
