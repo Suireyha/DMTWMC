f6b528d8e44f0d564bd5fc973eab7892c74d6d0e
WilliamMilne48231118 <william.milne@students.mq.edu.au>
2025-09-15 14:49:25 +1000

    Stage and state

--- src/Stage.java
+++ src/Stage.java
@@ -43,19 +43,7 @@ public class Stage {
   public void paint(Graphics g, Point mouseLoc) {
     // do we have bot moves to make?
 
-    if(currentState == State.BotMoving) {
-      for(Actor player: listOfPlayers) {
-        if(player.isBot()) {
-          List<Cell> possibleLocs = getClearRadius(player.loc, player.moves);
-          int moveBotChooses = (new Random()).nextInt(possibleLocs.size());
-          player.setLocation(possibleLocs.get(moveBotChooses));
-        }
-      }
-      currentState = State.ChoosingActor;
-      for(Actor player: listOfPlayers) {
-        player.turns = 1;
-      }
-    }
+    currentState.paint();
 
     grid.paint(g, mouseLoc);
     // Blue cell selection overlay with 50% transparency
@@ -114,45 +102,6 @@ public class Stage {
   }
 
   public void mouseClicked(int x, int y) {
-    switch(currentState) {
-      case ChoosingActor:
-        playerInAction = Optional.empty();
-        for(Actor player: listOfPlayers) {
-          if(player.loc.contains(x, y) && !player.isBot()) {
-            cellOverlay = grid.getRadius(player.loc, player.moves);
-            playerInAction = Optional.of(player);
-            currentState = State.SelectingNewLocation;
-          }
-        }
-        break;
-      case SelectingNewLocation:
-        Optional<Cell> clicked = Optional.empty();
-        for(Cell c: cellOverlay) {
-          if(c.contains(x, y)) {
-            clicked = Optional.of(c);
-          }
-        }
-        cellOverlay = new ArrayList<Cell>();
-        if(clicked.isPresent() && playerInAction.isPresent()) {
-          playerInAction.get().setLocation(clicked.get());
-          playerInAction.get().turns--;
-          int humansWithMovesLeft = 0;
-          for(Actor player: listOfPlayers) {
-            if(!player.isBot() && player.turns > 0) {
-              humansWithMovesLeft++;
-            }
-          }
-          if(humansWithMovesLeft > 0) {
-            currentState = State.ChoosingActor;
-          } else {
-            currentState = State.BotMoving;
-          }
-        }
-        break;
-      default:
-        // this state should never be reached
-        System.out.println(currentState);
-        break;
-    }
+    currentState.mouseClicked(x, y);
   }
 }
--- src/State.java
+++ src/State.java
@@ -1,3 +1,4 @@
 public interface State {
-    
+    public void paint();
+    public void mouseClicked(int x, int y);
 }
